288710fba778ce4cf37b53b9ddbf430335bdc50c
Mustafa D. Khalid <mcho.khalid@gmail.com>
2022-08-10 20:14:50 +0300

    widget to pick an image while signup

---
+++ lib/pickers/user_image_picker.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
+import 'dart:io';
+
+class UserImagePicker extends StatefulWidget {
+  const UserImagePicker({Key? key}) : super(key: key);
+
+  @override
+  State<UserImagePicker> createState() => _UserImagePickerState();
+}
+
+class _UserImagePickerState extends State<UserImagePicker> {
+  //global var picked image to use it in cirularAatar()
+  var _pickedImage;
+
+  //method to pick image
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedImageFile = await picker.pickImage(source: ImageSource.gallery);
+    setState(() {
+      _pickedImage = pickedImageFile;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        //adding user picture in login form
+        CircleAvatar(
+          backgroundColor: Colors.grey,
+          radius: 40,
+          backgroundImage:
+              _pickedImage != null ? FileImage(File(_pickedImage!.path)) : null,
+        ),
+        //button to take a photo
+        TextButton.icon(
+          onPressed: _pickImage,
+          icon: const Icon(Icons.image),
+          label: const Text('Add image'),
+        ),
+      ],
+    );
+  }
+}
